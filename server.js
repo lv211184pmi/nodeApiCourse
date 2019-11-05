f851f00dfa31592f47a5b34f9a98e27c320e19e1
lv211184pmi <lv21184pmi@gmail.com>
2019-11-05 15:26:28 +0200

    decouple code on routes and controllers

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const http = require('http');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Load env vars
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Bound routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
